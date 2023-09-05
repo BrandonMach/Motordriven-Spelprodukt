1031cc446b5a69e9bb16d745f1e59ac9bb8bd2e3
Bloodbone9829 <marcomatejic@gmail.com>
2023-09-05 17:28:00 +0200

    Working on movement script and input system

--- MotordirvenSpelprodukt/Assets/Scripts/MarcoScripts/GameInput.cs
+++ MotordirvenSpelprodukt/Assets/Scripts/MarcoScripts/GameInput.cs
@@ -1,20 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
-    private PlayerInputActions playerInputActions;
+    public event EventHandler OnAltbuttonPressed;
+
+    private PlayerInputActions _playerInputActions;
 
     private void Awake()
     {
-        playerInputActions = new PlayerInputActions();
-        playerInputActions.Player.Enable();
+        _playerInputActions = new PlayerInputActions();
+        _playerInputActions.Player.Enable();
+
+        _playerInputActions.Player.AltButton.performed += AltButton_performed;
+    }
+
+    private void AltButton_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        OnAltbuttonPressed?.Invoke(this, EventArgs.Empty);
     }
 
     public Vector2 GetMovementVectorNormalized()
     {
-        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+        Vector2 inputVector = _playerInputActions.Player.Move.ReadValue<Vector2>();
         return inputVector.normalized;
     }
 }
