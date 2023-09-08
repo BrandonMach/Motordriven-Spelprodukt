d33dd8d00e53b3336e9916a73680642e1d78e6f4
Bloodbone9829 <marcomatejic@gmail.com>
2023-09-08 15:00:51 +0200

    Revert "Revert "Revert "Working on movement script and input system"""
    This reverts commit 12fc7ab981e14eafe7316795051dce2a29a5a87f.

--- MotordirvenSpelprodukt/Assets/Scripts/MarcoScripts/GameInput.cs
+++ MotordirvenSpelprodukt/Assets/Scripts/MarcoScripts/GameInput.cs
@@ -1,30 +1,20 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
-    public event EventHandler OnAltbuttonPressed;
-
-    private PlayerInputActions _playerInputActions;
+    private PlayerInputActions playerInputActions;
 
     private void Awake()
     {
-        _playerInputActions = new PlayerInputActions();
-        _playerInputActions.Player.Enable();
-
-        _playerInputActions.Player.AltButton.performed += AltButton_performed;
-    }
-
-    private void AltButton_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
-    {
-        OnAltbuttonPressed?.Invoke(this, EventArgs.Empty);
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
     }
 
     public Vector2 GetMovementVectorNormalized()
     {
-        Vector2 inputVector = _playerInputActions.Player.Move.ReadValue<Vector2>();
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
         return inputVector.normalized;
     }
 }
