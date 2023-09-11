306e51aee8471797bc52d7d17dd64976ea504eb8
BrandonMach <brandon.mach2003@gmail.com>
2023-09-11 12:28:23 +0200

    testing combo tree UI

--- MotordirvenSpelprodukt/Assets/Scripts/BrandonScripts/ComboTestScript.cs
+++ MotordirvenSpelprodukt/Assets/Scripts/BrandonScripts/ComboTestScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using TMPro;
 
 public class ComboTestScript : MonoBehaviour
 {
@@ -9,7 +10,7 @@ public class ComboTestScript : MonoBehaviour
 
     [SerializeField] KeyCode[] _attackInputs; //Static attack inputs
 
-
+    [SerializeField] TextMeshProUGUI _comboTreeInfoText;
 
     [Header("Combo Sequence")]
     public KeyCode[] _inputSequence;
@@ -18,31 +19,30 @@ public class ComboTestScript : MonoBehaviour
 
     [SerializeField] private bool _startComboWindowTimer;
 
-    private ArrayList _comboList = new ArrayList();
 
     private List<KeyCode[]> cbList = new List<KeyCode[]>();
 
     EntertainmentManager _etpManager;
     public List<KeyCode> _lastUsedInputs = new List<KeyCode>();
 
+    string ComboTree;
+
     void Start()
     {
 
         _etpManager = GameObject.Find("Canvas").GetComponent<EntertainmentManager>();
-        //_comboList.Add((_attackInputs[0], _attackInputs[0], _attackInputs[1])); //Combo: V,V,B
-        //_comboList.Add((_attackInputs[0], _attackInputs[1], _attackInputs[2])); //Combo: V,B,N
-        //_comboList.Add((_attackInputs[3], _attackInputs[1], _attackInputs[3])); //Combo: M,V,M
+
+        cbList.Add(new KeyCode[] {_attackInputs[0], _attackInputs[0], _attackInputs[3]}); //Combo: V,V,M
+        cbList.Add(new KeyCode[] {_attackInputs[0], _attackInputs[1], _attackInputs[2]}); //Combo: V,B,N
+        cbList.Add(new KeyCode[] {_attackInputs[3], _attackInputs[0], _attackInputs[3]}); //Combo: M,V,M
 
 
-        cbList.Add(new KeyCode[] {_attackInputs[0], _attackInputs[0], _attackInputs[3]});
-        cbList.Add(new KeyCode[] {_attackInputs[0], _attackInputs[1], _attackInputs[2]});
-        cbList.Add(new KeyCode[] {_attackInputs[3], _attackInputs[0], _attackInputs[3]});
 
+        foreach (var item in cbList)
+        {
+            _comboTreeInfoText.text = item.ToString();
+        }
 
-        //foreach (var combos in _comboList)
-        //{
-        //    Debug.Log(combos);
-        //}
     }
 
     // Update is called once per frame
@@ -57,24 +57,23 @@ public class ComboTestScript : MonoBehaviour
                 _comboWindowTimer = 0;
 
 
-                if (_lastUsedInputs.Count == 3) //remove first inputed key
+                if (_lastUsedInputs.Count == 3) //remove first inputed key, Only 3 input lenght Combos
                 {
                     _lastUsedInputs.RemoveAt(0);
                 }
                 _lastUsedInputs.Add(_attackInputs[i]); //add latest key
 
 
-                KeyCode[] sdsda = _lastUsedInputs.ToArray();
+                KeyCode[] comboAttempt = _lastUsedInputs.ToArray();
 
-                Debug.Log(sdsda);
                 foreach (var Combos in cbList)
                 {
-                    if (Enumerable.SequenceEqual(Combos, sdsda))
+                    if (Enumerable.SequenceEqual(Combos, comboAttempt))
                     {
                         Debug.LogError("Combo matched");
                         _etpManager.increaseETP(15);
                     }
-                    Debug.Log(Combos.Equals(sdsda));
+                    
                 }
             }
         }
