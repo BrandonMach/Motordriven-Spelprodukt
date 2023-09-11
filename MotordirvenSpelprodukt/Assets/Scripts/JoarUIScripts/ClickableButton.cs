06b815ef297c416f406d681eef5c455c52931908
JHC1133 <joarhyltencavallius@gmail.com>
2023-09-11 11:06:38 +0200

    Made script for clickable buttons

---
+++ MotordirvenSpelprodukt/Assets/Scripts/JoarUIScripts/ClickableButton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+    [SerializeField] private Image _img;
+    [SerializeField] private Sprite _default, _pressed;
+    // TODO: add AudioClip _compressClip, _uncompressClip; add AudioSource _source : When click audio will be added as a feature
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _img.sprite = _pressed;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _img.sprite = _default;
+    }
+
+
+}
