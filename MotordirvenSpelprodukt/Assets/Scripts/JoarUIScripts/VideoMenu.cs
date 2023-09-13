63f59750c9a056d7130c13a69dcda81b544b7cd7
JHC1133 <joarhyltencavallius@gmail.com>
2023-09-13 19:19:30 +0200

    asdasd

--- MotordirvenSpelprodukt/Assets/Scripts/JoarUIScripts/VideoMenu.cs
+++ MotordirvenSpelprodukt/Assets/Scripts/JoarUIScripts/VideoMenu.cs
@@ -23,34 +23,34 @@ public class VideoMenu : MenuAbstract, IMenu
 
     void SettingResolutionDropDownContent()
     {
-        resolutions = Screen.resolutions;
+        //resolutions = Screen.resolutions;
 
-        resolutionDropDown.ClearOptions();
+        //resolutionDropDown.ClearOptions();
 
-        List<string> options = new List<string>();
+        //List<string> options = new List<string>();
 
-        int currentResoultionIndex = 0;
-        for (int i = 0; i < resolutions.Length; i++)
-        {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(option);
+        //int currentResoultionIndex = 0;
+        //for (int i = 0; i < resolutions.Length; i++)
+        //{
+        //    string option = resolutions[i].width + " x " + resolutions[i].height;
+        //    options.Add(option);
 
-            if (resolutions[i].width == Screen.currentResolution.width &&
-                resolutions[i].height == Screen.currentResolution.height)
-            {
-                currentResoultionIndex = i;
-            }
-        }
+        //    if (resolutions[i].width == Screen.currentResolution.width &&
+        //        resolutions[i].height == Screen.currentResolution.height)
+        //    {
+        //        currentResoultionIndex = i;
+        //    }
+        //}
 
-        resolutionDropDown.AddOptions(options);
-        resolutionDropDown.value = currentResoultionIndex;
-        resolutionDropDown.RefreshShownValue();
+        //resolutionDropDown.AddOptions(options);
+        //resolutionDropDown.value = currentResoultionIndex;
+        //resolutionDropDown.RefreshShownValue();
     }
 
     public void SetResoultion(int resolutionIndex)
     {
-        Resolution resolution = resolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        //Resolution resolution = resolutions[resolutionIndex];
+        //Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
     public void SetQuality(int qualityIndex)
