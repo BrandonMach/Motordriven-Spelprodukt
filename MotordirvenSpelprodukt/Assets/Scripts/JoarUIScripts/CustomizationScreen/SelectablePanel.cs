0f78142ad1a25859b5517329e62495447d36258b
JHC1133 <joarhyltencavallius@gmail.com>
2023-09-22 16:18:07 +0200

    asdasdasd

---
+++ MotordirvenSpelprodukt/Assets/Scripts/JoarUIScripts/CustomizationScreen/SelectablePanel.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectablePanel : MonoBehaviour
+{
+    [Header("Selectable Panel Items")]
+    [SerializeField] private GameObject _inventoryPanel;
+    [SerializeField] private GameObject _abilitiesPanel;
+    [SerializeField] private GameObject _challengesPanel;
+    [SerializeField] private GameObject _shopPanel;
+
+    private GameObject[] _panels;
+    private int _currentPanelIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _panels = new GameObject[]
+        {
+            _inventoryPanel,
+            _abilitiesPanel,
+            _challengesPanel,
+            _shopPanel
+        };
+    }
+
+    private void TogglePanel(int panelIndex)
+    {
+        for (int i = 0; i < _panels.Length; i++)
+        {
+            _panels[i].SetActive(i == panelIndex);
+        }
+    }
+
+    private void SetCurrentPanel(int panelIndex)
+    {
+        if (panelIndex >= 0 && panelIndex < _panels.Length)
+        {
+            _currentPanelIndex = panelIndex;
+            TogglePanel(_currentPanelIndex);
+        }
+        else
+        {
+            Debug.LogError("Invalid panel index");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void InventoryPanelClicked()
+    {
+        SetCurrentPanel(0);
+    }
+    public void AbilitiesPanelClicked()
+    {
+        SetCurrentPanel(1);
+    }
+
+    public void ChallengesPanelClicked()
+    {
+        SetCurrentPanel(2);
+    }
+
+    public void ShopPanelClicked()
+    {
+        SetCurrentPanel(3);
+    }
+}
---
+++ MotordirvenSpelprodukt/Assets/Scripts/JoarUIScripts/CustomizationScreen/SelectablePanelButton.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CustomButton : Button
+{
+    Color _normalColor = new Color(96, 96, 96);
+    Color _pressedColor = new Color(200, 200, 200);
+    bool _isSelected = false;
+    ColorBlock _colorsBlock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _colorsBlock = colors;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        base.OnPointerClick(eventData);
+
+        if (!_isSelected)
+        {
+            ButtonIsSelected();
+        }
+        else
+        {
+            ButtonIsUnSelected();
+        }
+
+    }
+
+    private void ButtonIsSelected()
+    {
+        _isSelected = true;
+        _colorsBlock.normalColor = _pressedColor;
+    }
+
+    private void ButtonIsUnSelected()
+    {
+        _isSelected = false;
+        _colorsBlock.normalColor = _normalColor;
+    }
+}
