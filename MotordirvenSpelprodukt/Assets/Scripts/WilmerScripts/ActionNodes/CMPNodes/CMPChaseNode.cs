aa8f741882f33fb96edf5b2fd288bb8d5eddc5ec
BrandonMach <brandon.mach2003@gmail.com>
2023-09-28 20:15:29 +0200

    added orc weapon and trying to add behaviour tree

--- MotordirvenSpelprodukt/Assets/Scripts/WilmerScripts/ActionNodes/CMPNodes/CMPChaseNode.cs
+++ MotordirvenSpelprodukt/Assets/Scripts/WilmerScripts/ActionNodes/CMPNodes/CMPChaseNode.cs
@@ -2,16 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CMPScript : EnemyScript
+public class CMPChaseNode : MonoBehaviour
 {
-    public float HealthPoints;
-
-    public float DamageBuff;
-
-    public float ChaseDistance;
-
-    public Animator anim;
-
+    // Start is called before the first frame update
     void Start()
     {
         
