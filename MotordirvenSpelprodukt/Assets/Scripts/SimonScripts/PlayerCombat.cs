88447dc0c17f0b2a6acb3b4a596529ff40c9e8ac
Laagren <89873758+Laagren@users.noreply.github.com>
2023-10-03 19:43:36 +0200

    Added particle effects

--- MotordirvenSpelprodukt/Assets/Scripts/SimonScripts/PlayerCombat.cs
+++ MotordirvenSpelprodukt/Assets/Scripts/SimonScripts/PlayerCombat.cs
@@ -7,11 +7,12 @@ public class PlayerCombat : MonoBehaviour
     [SerializeField] private Player player;
     private string _currentCombo;
     private float _range;
+    private float _impactPosOffset =1.5f;
     private float _damage;
     private float _multiplier;
     private Vector3 _colliderPos;
-    private bool _registerHit;
     private CurrentAttackSO.AttackEffect _effect;
+    private Vector3 _checkPos;
     [SerializeField] private ParticleSystem stunEffect;
 
     void Start()
@@ -22,58 +23,69 @@ public class PlayerCombat : MonoBehaviour
 
     private void Player_OnAttack(object sender, Player.OnAttackPressedEventArgs e)
     {
-        // Handle attack logic
-        // Check for collision with enemy
-        // Deal damage
-
         RecieveAttackEvent(e);
-
-        HandleAttack(e);
+        //HandleAttack(e);
     }
 
 
 
-    private void HandleAttack(Player.OnAttackPressedEventArgs e)
+    private void HandleAttack()
     {
-        if (_registerHit)
+        Gizmos.color = Color.red;
+        // Temporary fix until _range can be passed on via event.
+        // SHOULD BE REMOVED LATER.
+        switch (_effect)
         {
-            // TODO:
-            // Set animation event to the animations to check when an attack should check for collisions.
-            _effect = e.CurrentAttackSO.CurrentAttackEffect;
-
-            Collider[] enemyHits = Physics.OverlapSphere(transform.position + (transform.forward * _range) + (transform.up * transform.localScale.y), _range);
-
-            for (int i = 0; i < enemyHits.Length; i++)
-            {
-                IDamagable enemy = enemyHits[i].GetComponent<IDamagable>();
-                if (enemy != null)
-                {
-                    switch (_effect)
-                    {
-                        case CurrentAttackSO.AttackEffect.None:
-                            break;
-                        case CurrentAttackSO.AttackEffect.Pushback:
-                            break;
-                        case CurrentAttackSO.AttackEffect.bleed:
-
-                            break;
-                        case CurrentAttackSO.AttackEffect.AreaDamage:
-                            break;
-                        case CurrentAttackSO.AttackEffect.Stun:
-                            enemy.GetStunned(2.0f);
-                            Vector3 pos = (enemy as MonoBehaviour).transform.position;
-                            pos = new Vector3(pos.x, pos.y + 1, pos.z);
-                            Instantiate(stunEffect, pos, Quaternion.Euler(-90, 0, 0));
-                            break;
-                        default:
-                            break;
-                    }
-
-                    enemy.TakeDamage(1.0f);
-                    _registerHit = false;
-                }
-            } 
+            case CurrentAttackSO.AttackEffect.None:
+                break;
+            case CurrentAttackSO.AttackEffect.Pushback:
+                break;
+            case CurrentAttackSO.AttackEffect.bleed: // Should be swapped to None here and on L scriptable object.
+                // Normal attack, check infront of player.
+                // Sphere starts at front edge of player and diameter = _range
+                _range = 1.5f;
+                _checkPos = (transform.position + (transform.forward * _range) + (transform.up * transform.localScale.y));
+                break;
+            case CurrentAttackSO.AttackEffect.AreaDamage:
+            case CurrentAttackSO.AttackEffect.Stun:
+                // Area attack, check around player 
+                // Sphere is centered around player 
+                _range = 5;
+                _checkPos = (transform.position + (transform.forward * _impactPosOffset) + (transform.up * transform.localScale.y));
+                break;
+            default:
+                break;
         }
+
+        Collider[] enemyHits = Physics.OverlapSphere(_checkPos, _range);
+
+        for (int i = 0; i < enemyHits.Length; i++)
+        {
+            IDamagable enemy = enemyHits[i].GetComponent<IDamagable>();
+            if (enemy != null)
+            {
+                switch (_effect)
+                {
+                    case CurrentAttackSO.AttackEffect.None:
+                        break;
+                    case CurrentAttackSO.AttackEffect.Pushback:
+                        break;
+                    case CurrentAttackSO.AttackEffect.bleed:
+
+                        break;
+                    case CurrentAttackSO.AttackEffect.AreaDamage:
+                        break;
+                    case CurrentAttackSO.AttackEffect.Stun:
+                        HandelStun(enemy);
+                        break;
+                    default:
+                        break;
+                }
+
+                enemy.TakeDamage(1.0f);
+            }
+        }
+        Gizmos.color = Color.green;
     }
 
 
@@ -86,20 +98,29 @@ public class PlayerCombat : MonoBehaviour
         _multiplier = e.CurrentAttackSO.DamageMultiplier;
         _effect = e.CurrentAttackSO.CurrentAttackEffect;
     }
+    
 
-    private void OnDrawGizmos()
+    private void HandelStun(IDamagable enemy)
     {
-        Gizmos.color = Color.green;
-        //Vector3 drawPos = new Vector3(transform.position.)
-        Gizmos.DrawWireSphere(transform.position + (transform.forward * _range) + (transform.up * transform.localScale.y), _range);
+        enemy.GetStunned(2.0f);
+        Vector3 pos = (enemy as MonoBehaviour).transform.position;
+        pos = new Vector3(pos.x, pos.y + 1, pos.z);
+        Instantiate(stunEffect, pos, Quaternion.Euler(-90, 0, 0));
     }
+
 
     public void RegisterAttack()
     {
-        _registerHit = true;
+        HandleAttack();
     }
 
 
+    private void OnDrawGizmos()
+    {
+        //Gizmos.color = Color.green;
+        //Vector3 drawPos = new Vector3(transform.position.)
+        Gizmos.DrawWireSphere(_checkPos, _range);
+    }
     //private void HandleInput(Player.OnAttackPressedEventArgs e)
     //{
     //    switch (e.attackType1)
@@ -140,10 +161,4 @@ public class PlayerCombat : MonoBehaviour
     //        //attack.Attack(test[i]);
     //    }
     //}
-
-
-    void Update()
-    {
-
-    }
 }
