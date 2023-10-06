bd958ba24c2ffb4e75c9f66579c87ab09b90ccfb
BrandonMach <brandon.mach2003@gmail.com>
2023-10-07 01:08:55 +0200

    added slow mo+ kan kombineras med TimeMnager skriptet

---
+++ MotordirvenSpelprodukt/Assets/Scripts/BrandonScripts/SlowMo.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowMo: MonoBehaviour
+{
+    public float slowdownFactor = 0.05f;
+    public float slowDownDuration = 2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale += (1f / slowDownDuration) * Time.unscaledDeltaTime;
+        Time.fixedDeltaTime += (0.01f / slowDownDuration) * Time.unscaledDeltaTime;
+        Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+        Time.fixedDeltaTime = Mathf.Clamp(Time.fixedDeltaTime, 0f, 0.01f);
+    }
+
+    public void DoSlowmotion()
+    {
+        Time.timeScale = slowdownFactor;
+        Time.fixedDeltaTime = Time.timeScale * 0.02f;
+    }
+}
