a5b3fd8e9a8495da04a4cd5d74256fa66ae4be63
JHC1133 <joarhyltencavallius@gmail.com>
2023-10-11 20:58:28 +0200

    Before trying to merge with kristoffer

--- MotordirvenSpelprodukt/Assets/Scripts/JoarUIScripts/CustomizationScreen/InventoryUI.cs
+++ MotordirvenSpelprodukt/Assets/Scripts/JoarUIScripts/CustomizationScreen/InventoryUI.cs
@@ -2,13 +2,18 @@ using UnityEngine;
 
 public class InventoryUI : MonoBehaviour
 {
+
+    [SerializeField]Transform _itemsParent;
     Inventory _inventory;
+    InventorySlot[] _slots;
 
     // Start is called before the first frame update
     void Start()
     {
         _inventory = Inventory.Instance;
         _inventory.onItemChangedCallback += UpdateUI;
+
+        _slots = _itemsParent.GetComponentsInChildren<InventorySlot>();
     }
 
     // Update is called once per frame
@@ -19,6 +24,17 @@ public class InventoryUI : MonoBehaviour
 
     void UpdateUI()
     {
-        Debug.Log("Updating UI");
+
+        for (int i = 0; i < _slots.Length; i++)
+        {
+            if (i < _inventory.InventoryList.Count)
+            {
+                _slots[i].AddItem(_inventory.InventoryList[i]);
+            }
+            else
+            {
+                _slots[i].ClearSlot();
+            }
+        }
     }
 }
