37ab6d5b893ae8f1e652f2ced1ba026f359e4201
JHC1133 <joarhyltencavallius@gmail.com>
2023-11-06 10:37:07 +0100

    asdasd

--- MotordirvenSpelprodukt/Assets/Scripts/Audio/FMODController.cs
+++ MotordirvenSpelprodukt/Assets/Scripts/Audio/FMODController.cs
@@ -11,8 +11,9 @@ public class FMODController : MonoBehaviour
 
     GameManager _gameManager;
     HealthManager _healthManager;
+    EntertainmentManager _entertainmentManager;
 
-    public FMOD.Studio.EventInstance _fmodEventInstance;
+    public EventInstance _fmodEventInstance;
 
 
     float _intensity;
@@ -22,32 +23,75 @@ public class FMODController : MonoBehaviour
     void Start()
     {
         _gameManager = GameManager.Instance;
+        _entertainmentManager = EntertainmentManager.Instance;
 
         _fmodEventInstance = GetComponent<FMODUnity.StudioEventEmitter>().EventInstance;
 
-        
 
+        _health = 1;
+        _intensity = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        _health = healthSlider.value;
-        //Debug.Log(healthSlider.value);
+        UpdateIntensityParameter();
+        UpdateHealthParameter();
+        //Debug.Log("Float etp: " + _intensity);
 
-        _fmodEventInstance.setParameterByName("Health", _health);
-
-
-        // Debug print to check current paramValue for Health
-        if (Input.GetKey(KeyCode.Space))
+        // Debug print to check current paramValue for fmodproject intensity
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            _fmodEventInstance.getParameterByName("Health", out float changedParamValue);
+            _intensity = 0;
+            _fmodEventInstance.getParameterByName("Intensity", out float changedParamValue);
 
             Debug.Log($"ChangedParamValue: {changedParamValue}");
 
             Debug.Log(_fmodEventInstance);
             //Debug.Log(result);
         }
+
+        float etp = _entertainmentManager.GetETP();
+        Debug.Log("ETP: " + etp);
+
+        //if (Input.GetKeyDown(KeyCode.X))
+        //{
+        //    _intensity -= 0.33f;
+        //    _fmodEventInstance.getParameterByName("Intensity", out float changedParamValue);
+
+        //    Debug.Log($"ChangedParamValue: {changedParamValue}");
+        //}
+        //if (Input.GetKeyDown(KeyCode.Z))
+        //{
+        //    _intensity += 0.33f;
+        //    _fmodEventInstance.getParameterByName("Intensity", out float changedParamValue);
+
+        //    Debug.Log($"ChangedParamValue: {changedParamValue}");
+        //}
+        //if (Input.GetKeyDown(KeyCode.C))
+        //{
+        //    _intensity = _entertainmentManager.GetETPThreshold();
+        //    _fmodEventInstance.getParameterByName("Intensity", out float changedParamValue);
+
+        //    Debug.Log($"ChangedParamValue: {changedParamValue}");
+        //}
+    }
+
+    private void UpdateIntensityParameter()
+    {
+        _intensity = _entertainmentManager.GetETP();
+        _fmodEventInstance.setParameterByName("Intensity", _intensity);
+
+        _fmodEventInstance.getParameterByName("Intensity", out float changedParamValue);
+
+        //Debug.Log($"ChangedParamValue: {changedParamValue}");
+    }
+
+    private void UpdateHealthParameter()
+    {
+        //_health = healthSlider.value;
+        _fmodEventInstance.setParameterByName("Health", _health);
+
     }
 
 
