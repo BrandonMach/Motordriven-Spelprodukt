a13df6f1b133a30065d83c0a19a00942eee07a9f
BrandonMach <brandon.mach2003@gmail.com>
2023-11-13 21:21:59 +0100

    kamikaze automatic + manual explode

--- MotordirvenSpelprodukt/Assets/Scripts/WilmerScripts/ActionNodes/KMNodes/KMAutomaticExplode.cs
+++ MotordirvenSpelprodukt/Assets/Scripts/WilmerScripts/ActionNodes/KMNodes/KMAutomaticExplode.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KMExplode : ActionNode
+public class KMAutomaticExplode : ActionNode
 {
     protected override void OnStart()
     {
@@ -14,15 +14,15 @@ public class KMExplode : ActionNode
 
     protected override void OnStop()
     {
-        
+
     }
 
     protected override State OnUpdate()
     {
-        if(Vector3.Distance(_kamikazeScript.transform.position, _playerScript.transform.position) <= _kamikazeScript._automaticExplodeRange)
+        if (Vector3.Distance(_kamikazeScript.transform.position, _playerScript.transform.position) <= _kamikazeScript._automaticExplodeRange && !_kamikazeScript._maunalExplodeActive)
         {
-            _kamikazeScript.Anim.Play("Dive");
-            //Debug.Log("Kamikaze");
+            _kamikazeScript._maunalExplodeActive = true;
+            Debug.Log("Automatic explode is active");
             return State.Success;
         }
         else
--- MotordirvenSpelprodukt/Assets/Scripts/WilmerScripts/ActionNodes/KMNodes/KMStartManualExplode.cs
+++ MotordirvenSpelprodukt/Assets/Scripts/WilmerScripts/ActionNodes/KMNodes/KMStartManualExplode.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KMExplode : ActionNode
+public class KMStartManualExplode : ActionNode
 {
     protected override void OnStart()
     {
@@ -14,15 +14,15 @@ public class KMExplode : ActionNode
 
     protected override void OnStop()
     {
-        
+
     }
 
     protected override State OnUpdate()
     {
-        if(Vector3.Distance(_kamikazeScript.transform.position, _playerScript.transform.position) <= _kamikazeScript._automaticExplodeRange)
+        if (Vector3.Distance(_kamikazeScript.transform.position, _playerScript.transform.position) <= _kamikazeScript._activateManualExplodeRange)
         {
             _kamikazeScript.Anim.Play("Dive");
-            //Debug.Log("Kamikaze");
+            Debug.Log("Kamikaze");
             return State.Success;
         }
         else
