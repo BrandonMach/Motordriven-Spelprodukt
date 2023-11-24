a764ff709336f464fdb3d7c6e483b21acbea30c0
BrandonMach <brandon.mach2003@gmail.com>
2023-11-24 13:59:33 +0100

    s

---
+++ MotordirvenSpelprodukt/Assets/Scripts/BrandonScripts/AttackTreeVideoManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using TMPro;
+using System.IO;
+
+public class AttackTreeVideoManager : MonoBehaviour
+{
+
+    [SerializeField] private VideoPlayer _videoClip;
+    [SerializeField] private TextMeshProUGUI _description;
+
+    public VideoClip[] AttackClips;
+    //Lägg till path till vapen typ eller vapen
+    public string Path;
+    
+    public int ComboIndex;
+
+    string[] lines;
+
+    void Start()
+    {
+
+        lines = File.ReadAllLines(Path);
+        foreach (var item in lines)
+        {
+            Debug.Log(item);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Path = GameObject.FindObjectOfType<TransferableScript>().GetWeapon().GetAttackTreeDescriptionPath();    
+        //lines = File.ReadAllLines(Path);
+
+    }
+
+    public void SwitchVideoClip(int comboIndex)
+    {
+        ComboIndex = comboIndex;
+
+        _description.text = lines[comboIndex];
+        _videoClip.clip = AttackClips[comboIndex];
+
+
+
+    }
+}
---
+++ MotordirvenSpelprodukt/Assets/Scripts/UI Scripts/UIStats.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIStats : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI damageText;
+    [SerializeField] TextMeshProUGUI coinsText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateCoinsText();
+        UpdateDamageText();
+    }
+
+    private void UpdateCoinsText()
+    {
+        if (GameManager.Instance != null)
+        {
+            coinsText.text = GameManager.PlayerCoins.ToString();
+        }
+        else
+        {
+            coinsText.text = "0";
+        }
+    }
+
+    private void UpdateDamageText()
+    {
+        if (Player.Instance != null)
+        {
+            damageText.text = Player.Instance.CurrentWeapon.GetDamage().ToString();
+        }
+    }
+}
