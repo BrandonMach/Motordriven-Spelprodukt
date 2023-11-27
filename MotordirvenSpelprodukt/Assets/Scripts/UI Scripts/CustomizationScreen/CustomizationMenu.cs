6e2a6d834010f0915fed3f3d47fc2d3db4ddd4b1
BrandonMach <brandon.mach2003@gmail.com>
2023-11-27 15:33:48 +0100

    button rotates player in customization

--- MotordirvenSpelprodukt/Assets/Scripts/UI Scripts/CustomizationScreen/CustomizationMenu.cs
+++ MotordirvenSpelprodukt/Assets/Scripts/UI Scripts/CustomizationScreen/CustomizationMenu.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 public class CustomizationMenu : MonoBehaviour
 {
     [SerializeField] ChallengeManager challengeManager;
+    public GameObject PlayerObject;
+    float Angle = 45;
 
     // Start is called before the first frame update
     void Start()
@@ -21,12 +23,44 @@ public class CustomizationMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Angle = Mathf.SmoothDampAngle(transform.eulerAngles.z, TargetRotation, ref r, 0.1f);
+
+        //PlayerObject.transform.rotation = Quaternion.Euler(0, Angle, 0);
     }
 
     public void ClickEnterArena()
     {
 
         SceneManager.LoadScene(2,LoadSceneMode.Single);
+    }
+
+    public void RotateLeftButton()
+    {
+
+        StartCoroutine(Rotate(Angle));
+    } 
+    public void RotateRightButton()
+    {
+        StartCoroutine(Rotate(-Angle));
+    }
+
+
+    IEnumerator Rotate(float angle, float duration = 1.0f)
+    {
+        Quaternion from = PlayerObject.transform.rotation;
+        Quaternion to = PlayerObject.transform.rotation;
+
+        to *= Quaternion.Euler(new Vector3(0, 1, 0) * angle);
+
+        float elapse = 0.0f;
+
+        while(elapse < duration)
+        {
+            PlayerObject.transform.rotation = Quaternion.Slerp(from, to, elapse / duration);
+            elapse += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.rotation = to;
     }
 }
