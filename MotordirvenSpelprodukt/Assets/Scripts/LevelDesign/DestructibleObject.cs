52de7c7191e5e8b498e6ccfc1c9f71c947628487
D. Kuan <dickkuan006@gmail.com>
2023-12-08 16:07:16 +0100

    Update DestructibleObject.cs

--- MotordirvenSpelprodukt/Assets/Scripts/LevelDesign/DestructibleObject.cs
+++ MotordirvenSpelprodukt/Assets/Scripts/LevelDesign/DestructibleObject.cs
@@ -8,15 +8,33 @@ public class DestructibleObject : MonoBehaviour, IDamagable
     [SerializeField] private GameObject referencePrefab;
     [SerializeField] private GameObject destroyedPrefab;
     [SerializeField] private float removeDelay = 10;
+    [SerializeField] private float removePhysicsDelay = 4;
 
     [Header("Debug")]
     [SerializeField] private float hp = 20;
+
+    Rigidbody[] rbsFragments;
+    MeshCollider[] mcFragments;
 
     void Awake()
     {
         referencePrefab.SetActive(true);
         destroyedPrefab.SetActive(false);
+
+        rbsFragments = GetComponentsInChildren<Rigidbody>();
+        mcFragments = GetComponentsInChildren<MeshCollider>();
     }
+
+    // ONLY FOR DEBUG PURPOSES
+    //public void Update()
+    //{
+    //    if (hp <= 0)
+    //    {
+    //        DestroyObjectWithDelay();
+    //        StartCoroutine(DisablePhysicsWithDelay());
+    //    }
+    //}
+
 
     public void ReceiveDamage(float damage)
     {
@@ -34,7 +52,23 @@ public class DestructibleObject : MonoBehaviour, IDamagable
     public void TakeDamage(Attack attack)
     {
         hp -= attack.Damage;
-        if (hp <= 0) DestroyObjectWithDelay();
+        if (hp <= 0)
+        {
+            DestroyObjectWithDelay();
+            StartCoroutine(DisablePhysicsWithDelay());
+        }
+    }
+
+    IEnumerator DisablePhysicsWithDelay()
+    {
+        yield return new WaitForSeconds(removePhysicsDelay);
+        Debug.Log("Removing physics from destructibles");
+
+        foreach (Rigidbody rb in rbsFragments)
+            Destroy(rb);
+
+        foreach(MeshCollider coll in mcFragments)
+            Destroy(coll);
     }
 }
 
