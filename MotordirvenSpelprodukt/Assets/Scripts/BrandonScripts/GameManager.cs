303d22bd3f2c5b19f66c5d6eff8472d2f5395805
BrandonMach <brandon.mach2003@gmail.com>
2023-12-11 19:20:23 +0100

    save

--- MotordirvenSpelprodukt/Assets/Scripts/BrandonScripts/GameManager.cs
+++ MotordirvenSpelprodukt/Assets/Scripts/BrandonScripts/GameManager.cs
@@ -162,7 +162,6 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Arean index" + ArenaLayoutIndex);
 
         if(Player.Instance != null)
         {
