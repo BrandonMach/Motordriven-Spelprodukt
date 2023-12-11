d0e2ee626ae9d33317abc0d3235da833b65ff630
chiyuuarchive <85938105+chiyuuarchive@users.noreply.github.com>
2023-12-11 18:00:44 +0100

    The gates will descend as the battle starts

---
+++ MotordirvenSpelprodukt/Assets/Scripts/LevelDesign/Gates.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+
+public class Gates : MonoBehaviour
+{
+    [SerializeField] float descendDelay = 0.5f;
+    [SerializeField] float descendHeight = -2.5f;
+    [SerializeField] float descendVelocity = 7.5f;
+
+    void Start() => StartCoroutine(DescendGatesDelay());
+
+    IEnumerator DescendGatesDelay()
+    {
+        yield return new WaitForSeconds(descendDelay);
+        float height = 0;
+        while (height > descendHeight)
+        {
+            float downVelocity = descendVelocity * Time.fixedDeltaTime;
+            transform.position = transform.position - new Vector3(0, downVelocity, 0);
+            height = transform.localPosition.y;
+            yield return new WaitForSeconds(Time.fixedDeltaTime);
+            Debug.Log(height);
+        }
+    }
+}
