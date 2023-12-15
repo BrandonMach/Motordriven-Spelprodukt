11af7557b02b8e86e4b4b4cb1b97c860633e945a
chiyuuarchive <85938105+chiyuuarchive@users.noreply.github.com>
2023-12-15 14:23:55 +0100

    fixed a gate bug (moving slowly during low fps)

--- MotordirvenSpelprodukt/Assets/Scripts/LevelDesign/Gates.cs
+++ MotordirvenSpelprodukt/Assets/Scripts/LevelDesign/Gates.cs
@@ -5,7 +5,7 @@ public class Gates : MonoBehaviour
 {
     [SerializeField] float descendDelay = 0.5f;
     [SerializeField] float descendHeight = -2.5f;
-    [SerializeField] float descendVelocity = 7.5f;
+    [SerializeField] float descendVelocity = 15f;
 
     void Start() => StartCoroutine(DescendGatesDelay());
 
@@ -15,10 +15,10 @@ public class Gates : MonoBehaviour
         float height = 0;
         while (height > descendHeight)
         {
-            float downVelocity = descendVelocity * Time.fixedDeltaTime;
+            float downVelocity = descendVelocity * Time.deltaTime;
             transform.position = transform.position - new Vector3(0, downVelocity, 0);
             height = transform.localPosition.y;
-            yield return new WaitForSeconds(Time.fixedDeltaTime);
+            yield return new WaitForSeconds(Time.deltaTime);
         }
     }
 }
