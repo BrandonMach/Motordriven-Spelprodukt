fba11df2bd76d165d9668f1a030ac14b6411f34b
BrandonMach <brandon.mach2003@gmail.com>
2024-01-05 21:55:07 +0100

    save

--- MotordirvenSpelprodukt/Assets/Scripts/KristofferScripts/Tooltip/TooltipTrigger.cs
+++ MotordirvenSpelprodukt/Assets/Scripts/KristofferScripts/Tooltip/TooltipTrigger.cs
@@ -2,11 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.UI;
 
 public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public string content;
     public string header;
+
+    public RectTransform reT;
+    [SerializeField] private VirtualMouseInput _virtualMouseInput;
+
+    private void Start()
+    {
+        reT = GetComponent<RectTransform>();
+        _virtualMouseInput = FindAnyObjectByType<VirtualMouseInput>();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+
     public void OnPointerEnter(PointerEventData eventData)
     {
         TooltipSystem.Show(header,content);
@@ -16,4 +33,7 @@ public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitH
     {
         TooltipSystem.Hide();
     }
+
+    
+    
 }
